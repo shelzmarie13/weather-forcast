55d14f5edcd64ae7b999d40a6ce83f7b09379918
shelzmarie13 <sm.oson13@gmail.com>
2021-08-18 18:27:50 +0800

    feature: add title

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,14 @@
 import React from 'react'
 import SearchCity from '@components/SearchCity'
 import WeatherForecast from '@components/WeatherForecast'
+import Head from 'next/head'
 
 const Home: React.FC = () => {
   return (
     <>
+    <Head>
+        <title>Weather Forecast</title>
+      </Head>
     <SearchCity />
     <WeatherForecast/>
     </>
